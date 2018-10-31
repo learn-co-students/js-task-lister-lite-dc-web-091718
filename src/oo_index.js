ac4383d4b854c722dfba9b66159eb38d00f515f0
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-10-31 14:35:45 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,37 @@
 
+
+let taskList =""
+
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  const domForm = document.getElementById("create-task-form");
+  const domDescription = document.getElementById("new-task-description");
+  const domPriority = document.getElementById("new-task-priority");
+  const taskUl = document.getElementById("tasks");
+  domForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createNewTask(domDescription.value);
+  });
+
+  function createNewTask(description){
+    console.log("add")
+    taskList = taskList + renderTask(description)
+    taskUl.innerHTML = taskList
+  }
+  function renderTask(description) {
+      return `
+        <li>
+          ${description}
+          <button data-description="${description}">X</button>
+        </li>
+        `;
+    }
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      console.log("remove")
+      ((e.toElement).parentElement).remove()
+      taskList = taskUl.innerHTML
+    }
+  })
+
+
 });
